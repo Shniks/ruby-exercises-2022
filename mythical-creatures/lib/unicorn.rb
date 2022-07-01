c9ae6b2b717602f99172aff5563cfc4934b56ad8
Nikhil Shahi <shniks@gmail.com>
2022-07-01 12:13:00 -0700

    Completed unicorn tests.

---
+++ mythical-creatures/lib/unicorn.rb
@@ -0,0 +1,19 @@
+class Unicorn
+
+  attr_reader :name,
+              :color
+
+  def initialize(name, color = 'silver')
+    @name = name
+    @color = color
+  end
+
+  def silver?
+    color == 'silver'
+  end
+
+  def say(something)
+    "**;* #{something} **;*"
+  end
+
+end
