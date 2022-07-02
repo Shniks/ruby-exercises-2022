f543180455d0197727bf5353bf850c1b715686ae
Nikhil Shahi <shniks@gmail.com>
2022-07-01 17:49:53 -0700

    Completed methods for Pirate class.

---
+++ mythical-creatures/lib/pirate.rb
@@ -0,0 +1,28 @@
+class Pirate
+
+  attr_reader :name,
+              :job,
+              :count,
+              :booty
+
+  def initialize(name, job = 'Scallywag')
+    @name = name
+    @job = job
+    @count = 0
+    @booty = 0
+  end
+
+  def cursed?
+    return true if @count >= 3
+    false
+  end
+
+  def commit_heinous_act
+    @count += 1
+  end
+
+  def rob_ship
+    @booty += 1
+  end
+
+end
